a4602a1f20ec577e68231a12200a1db9bf2079d0
Roman Stankevych <romandionus@gmail.com>
2021-12-15 22:25:19 +0200

    v1

---
+++ test.js
@@ -0,0 +1,23 @@
+"use strict";
+// 1
+// const productName = 'Droid'
+// console.log(productName);
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+
+// 2
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem = 3500; 
+
+// console.log(typeof productName);
+// console.log(typeof pricePerItem);
+// console.log(productName);
+// console.log(pricePerItem);
+
+// 3
+
+
